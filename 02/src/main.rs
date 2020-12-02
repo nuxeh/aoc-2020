0099c277c6de001067e9e7f69efa8725bb1c9f74
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-02 10:31:34 +0000

    Work on parsing

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,5 +1,22 @@
 use aocf::Aoc;
 
+#[derive(Default, Debug)]
+struct PassSpec {
+    range_from: u32,
+    range_to: u32,
+    letter: char,
+    password: String,
+}   
+
+impl PassSpec {
+    fn from_str(s: &str) -> Self {
+        let mut r = Self::default();
+        let parts: Vec<&str> = s.split(' ').collect();
+        let range: Vec<u32> = parts[0].split('-').map(|s| s.parse().ok()).flatten().collect();
+        r
+    }
+}
+
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
@@ -9,5 +26,11 @@ fn main() {
 
     let input = aoc.get_input(false);
 
-    println!("{:?}", input);
+    if let Ok(i) = input {
+        let v: Vec<PassSpec> = i.lines()
+            .map(PassSpec::from_str)
+            .collect();
+
+        println!("{:?}", v);
+    }
 }
