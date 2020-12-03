adc77fa2d22686eee3a369757e07c227f5c98859
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-03 11:03:33 +0000

    Refactor as fn

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -1,5 +1,26 @@
 use aocf::Aoc;
 
+fn count_trees(i: &str, right: usize, down: usize) -> usize {
+    let mut trees = 0;
+
+    let v: Vec<Vec<bool>> = i.lines()
+        .map(|l| {
+            l.repeat(200).chars().map(|c| c == '#').collect()
+        })
+        .collect();
+
+    v.iter()
+        .enumerate()
+        .for_each(|(y, l)| l.chunks_exact(right)
+                  .skip(y)
+                  .take(1)
+                  .for_each(|c| if c[0] {
+                      trees += 1
+                  }));
+
+    trees
+}
+
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
@@ -10,24 +31,10 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
-        let mut trees = 0;
-
-        let v: Vec<Vec<bool>> = i.lines()
-            .map(|l| {
-                l.repeat(200).chars().map(|c| c == '#').collect()
-            })
-            .collect();
 
         //println!("{:#?}", v);
         
-        v.iter()
-            .enumerate()
-            .for_each(|(y, l)| l.chunks_exact(3)
-                      .skip(y)
-                      .take(1)
-                      .for_each(|c| if c[0] {
-                          trees += 1
-                      }));
+        let trees = count_trees(&i, 3, 1);
 
         println!("number of trees: {}", trees);
     }
