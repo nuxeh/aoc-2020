61cd7cf3d1ee471d7752cefdcbc6f38a13e14bdc
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-03 10:04:02 +0000

    Add day 3

---
+++ 03/src/main.rs
@@ -0,0 +1,21 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(2))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        let mut trees = 0;
+
+        let v: Vec<Vec<bool>> = i.lines()
+            .map(|l| l.chars().map(|c| c == '#').collect())
+            .collect();
+
+        println!("number of trees: {}", trees);
+    }
+}
