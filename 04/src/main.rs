918ecdac87756ab036be02001775de60f7747943
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-04 11:08:12 +0000

    Lose momentum on part 2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     if let Ok(i) = input {
         let mut valid = 0;
 
-        println!("{:#?}", i.lines().collect::<Vec<&str>>());
+        //println!("{:#?}", i.lines().collect::<Vec<&str>>());
 
         let passports = i.lines()
             .fold(vec![HashMap::new()], |mut acc, v| {
@@ -40,7 +40,39 @@ fn main() {
             .filter(|p| p.len() == 7 && !p.contains_key("cid"))
             .count();
 
-        println!("{:#?}", passports);
+        //println!("{:#?}", passports);
+        println!("number of valid passports: {}", valid);
+
+        // Part 2
+
+        valid = passports.iter()
+            .filter(|p| check_valid(p))
+            .count();
+
         println!("number of valid passports: {}", valid);
     }
+}
+
+/*
+• byr (Birth Year) - four digits; at least 1920 and at most 2002.
+• iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+• eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+• hgt (Height) - a number followed by either cm or in:
+    If cm, the number must be at least 150 and at most 193.
+    If in, the number must be at least 59 and at most 76.
+• hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+• ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+• pid (Passport ID) - a nine-digit number, including leading zeroes.
+• cid (Country ID) - ignored, missing or not.
+*/
+
+fn check_valid(map: &HashMap<&str, &str>) -> bool {
+    match map.get("byr") {
+        Some(v) if v.parse::<u32>.is_some() => {
+            if let Ok(w) = v.parse() && w >= 1920 && w <= 2002 {
+
+        },
+        _ => return false,
+    }
+    true
 }
