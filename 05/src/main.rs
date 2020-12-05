d8e7842f84a7ce8c962d9ac0ad6a94af09056b4e
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-05 13:01:25 +0000

    ...

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,4 +1,5 @@
 use aocf::Aoc;
+use std::collections::HashSet;
 
 fn main() {
     let mut aoc = Aoc::new()
@@ -10,7 +11,31 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        // Part 1
         println!("{:?}", i.lines().map(get_id).max());
+
+        // Part 2
+        let seats: HashSet<_> = (0..(127*8)).collect();
+
+        let taken = i.lines()
+            .map(get_id)
+            .fold(HashSet::new(), |mut acc, v| {
+                acc.insert(v);
+                acc
+            });
+
+        println!("{:#?}", seats.difference(&taken));
+
+        
+
+            /*
+            .fold(vec![0usize, 8 * 127], |acc, v| {
+                acc.get_mut(v).and_then(|s| {
+                    *s = 1;
+                    acc
+                })
+            });
+            */
     }
 }
 
@@ -21,7 +46,7 @@ fn scale(r: (u32, u32), c: char) -> (u32, u32) {
         'B' => (r.0 + r2 + 1, r.1),
         _ => r,
     };
-    println!("{} {} {:?}", c, r2, ret);
+    //println!("{} {} {:?}", c, r2, ret);
     ret
 }
 
@@ -38,7 +63,7 @@ fn get_id(pass: &str) -> u32 {
         .take(3)
         .fold((0, 7), scale);
 
-    println!("{} {} {} {}", pass, r.0, c.0, r.0 * 8 + c.0);
+    //println!("{} {} {} {}", pass, r.0, c.0, r.0 * 8 + c.0);
 
     r.0 * 8 + c.0
 }
