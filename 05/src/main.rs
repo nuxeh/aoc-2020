b32be1fdbcbb35c56e2ed1b079a64a79e8627860
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-05 10:35:56 +0000

    Start day 5

---
+++ 05/src/main.rs
@@ -0,0 +1,19 @@
+use aocf::Aoc;
+use std::collections::HashMap;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(5))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+        let mut valid = 0;
+
+        println!("{:#?}", i.lines().collect::<Vec<&str>>());
+
+    }
+}
