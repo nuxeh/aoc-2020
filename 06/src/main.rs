1fd908dacad46b59600af25b509a9168d96e87f9
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-06 11:03:12 +0000

    Start day 6

---
+++ 06/src/main.rs
@@ -0,0 +1,15 @@
+use aocf::Aoc;
+use std::collections::HashSet;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(6))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+    }
+}
