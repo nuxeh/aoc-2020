52412ed3b82849fe5e51c3b2d5d9bdb656ca4f86
Ed Cragg <drq.11235@gmail.com>
2020-12-08 09:35:40 +0000

    Prepare 8

---
+++ 08/src/main.rs
@@ -0,0 +1,15 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(8))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+
+    }
+}
