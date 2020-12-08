40d69f89ffae20bf9492b2a081a9c06062080939
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-08 10:54:04 +0000

    Solve part 1

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -1,17 +1,39 @@
 use aocf::Aoc;
+use std::collections::HashSet;
 
+#[derive(Debug, Clone)]
 struct Cpu {
-    pc: usize,
-    acc: i32,
+    pub pc: i32,
+    pub acc: i32,
+    mem: Vec<(Op, i32)>,
 }
 
 impl Cpu {
-    fn ins(&mut self, ins: &(Op, i32) {
+    fn new(mem: Vec<(Op, i32)>) -> Self {
+        Self { pc: 0, acc: 0, mem }
+    }
 
+    fn exec(&mut self, ins: &(Op, i32)) {
+        match ins.0 {
+            Op::Acc => {
+                self.acc += ins.1;
+                self.pc += 1
+            },
+            Op::Jmp => self.pc += ins.1,
+            Op::Nop => self.pc += 1,
+            _ => (),
+        }
+    }
+
+    fn tick(&mut self) -> Option<()> {
+        // Eww
+        let op = &self.clone().mem[self.pc as usize];
+        self.exec(op);
+        Some(())
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Op {
     Acc,
     Jmp,
@@ -51,6 +73,20 @@ fn main() {
             .collect();
 
         println!("{:#?}", ins);
+
+        let mut cpu = Cpu::new(ins);
+        let mut used: HashSet<i32> = HashSet::new();
+
+        loop {
+            cpu.tick();
+            println!("{:?}", cpu);
+
+            if used.contains(&cpu.pc) {
+                break;
+            } else {
+                used.insert(cpu.pc);
+            }
+        }
 
     }
 }
