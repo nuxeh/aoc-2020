39eb5fc85e6c3f557d5f03e05acf3594b9588617
Ed Cragg <drq.11235@gmail.com>
2020-12-08 09:50:38 +0000

    Prepare

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -1,5 +1,35 @@
 use aocf::Aoc;
 
+enum Op {
+    Acc,
+    Jmp,
+    Nop,
+    None,
+}
+
+impl From<&str> for Op {
+    fn from(item: &str) -> Self {
+        match item {
+            "acc" => Self::Acc,
+            "jmp" => Self::Acc,
+            "nop" => Self::Acc,
+            _ => Self::None,
+        }
+    }
+}
+
+/*
+acc +17
+jmp +1
+acc +16
+acc +15
+jmp +161
+acc +37
+acc +5
+acc -13
+nop +134
+jmp +426
+*/
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
@@ -10,6 +40,10 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        let ins: (Op, i32) = i.lines()
+            .map(|v| v.split(' ').collect::Vec<&str>())
+            .map(|v| (v.iter.nth(0).unwrap().into(), v.iter.nth(1).unwrap().parse()));
+
 
     }
 }
