c159a1cae1781dda5d0f65ea76f95a344a6eb27e
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-10 11:50:46 +0000

    ...

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -3,13 +3,19 @@ use aocf::Aoc;
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
-        .day(Some(5))
+        .day(Some(10))
         .init()
         .unwrap();
 
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        let joltage_adapters: Vec<u16> = i.lines()
+            .map(|v| v.parse())
+            .flatten()
+            .collect();
+
+        println!("{:#?}", joltage_adapters);
 
     }
 }
