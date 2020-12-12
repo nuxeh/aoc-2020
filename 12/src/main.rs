31952d31e172431f4c07d5ba5535ccf7143022eb
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-12 10:35:00 +0000

    Parse
    [
        Action {
            dir: Forward,
            dist: 10,
        },
        Action {
            dir: North,
            dist: 3,
        },
        Action {
            dir: Forward,
            dist: 7,
        },
        Action {
            dir: Right,
            dist: 90,
        },
        Action {
            dir: Forward,
            dist: 11,
        },
    ]

--- 12/src/main.rs
+++ 12/src/main.rs
@@ -1,5 +1,48 @@
 use aocf::Aoc;
 
+#[derive(Debug, Clone, Copy)]
+struct Action {
+    dir: Direction,
+    dist: u32,
+}
+
+impl Action {
+    fn from_str(s: &str) -> Self {
+        let mut chars = s.chars();
+        Self {
+            dir: Direction::from_char(chars.next().unwrap()),
+            dist: chars.collect::<String>().parse().unwrap(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    None,
+    North,
+    South,
+    East,
+    West,
+    Left,
+    Right,
+    Forward,
+}
+
+impl Direction {
+    fn from_char(c: char) -> Self {
+        match c {
+            'N' => Self::North,
+            'S' => Self::South,
+            'E' => Self::East,
+            'W' => Self::West,
+            'L' => Self::Left,
+            'R' => Self::Right,
+            'F' => Self::Forward,
+            _ => Self::None,
+        }
+    }
+}
+
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
@@ -10,6 +53,11 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        let actions: Vec<_> = i
+            .lines()
+            .map(Action::from_str)
+            .collect();
 
+        println!("{:#?}", actions);
     }
 }
