8c02c43a6b02469b610f9c0d89c7035ac3dadc17
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-13 12:05:28 +0000

    Parse
    departure 1013728
    buses     [23, 41, 733, 13, 17, 19, 29, 449, 37]

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -3,13 +3,26 @@ use aocf::Aoc;
 fn main() {
     let mut aoc = Aoc::new()
         .year(Some(2020))
-        .day(Some(10))
+        .day(Some(13))
         .init()
         .unwrap();
 
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        let depart_at: u32 = i.lines().nth(0).unwrap().parse().unwrap();
+        let buses: Vec<u32> = i.lines().nth(1).unwrap()
+            .split(',')
+            .filter(|i| !i.contains('x'))
+            .map(|v| v.parse())
+            .flatten()
+            .collect();
 
+        println!("departure {}", depart_at);
+        println!("buses     {:?}", buses);
+
+        part_1();
     }
 }
+
+fn part_1() {}
