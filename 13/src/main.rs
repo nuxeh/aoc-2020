978697c6d6e1aab7ea54fbc609f10d46e3718602
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-13 16:41:33 +0000

    Recreate test result

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -48,17 +48,30 @@ fn part_1(dep: u32, buses: &[u32]) {
 fn part_2(buses: &[Option<usize>]) {
     let mut t: usize = 0;
     let num_buses = buses.len();
+    let num_valid_buses = buses.iter().flatten().count();
+
+    println!("evaluating part 2.\nbus count: {} valid bus count: {}",
+             num_buses, num_valid_buses);
 
     loop {
+        let mut count = 0;
+
         for (n, u) in (t..(t+num_buses)).enumerate() {
-            if let Some(b) = buses
+            if let Some(_) = buses
                 .iter()
                 .nth(n)
                 .map(|b| *b)
                 .flatten()
                 .filter(|b| u % b == 0) {
-                    println!("{} {}", b, t);
+                    //println!("{} {} {}", b, u, u % b);
+                    count += 1;
                 };
+        }
+
+        //println!("{} {}", t, count);
+
+        if count == num_valid_buses {
+            break;
         }
 
         t += 1;
