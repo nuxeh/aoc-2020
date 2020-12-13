d1bf59a90b0134a4a6891888c4a32131da17177b
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-13 11:57:03 +0000

    Start 13

---
+++ 13/src/main.rs
@@ -0,0 +1,15 @@
+use aocf::Aoc;
+
+fn main() {
+    let mut aoc = Aoc::new()
+        .year(Some(2020))
+        .day(Some(10))
+        .init()
+        .unwrap();
+
+    let input = aoc.get_input(false);
+
+    if let Ok(i) = input {
+
+    }
+}
