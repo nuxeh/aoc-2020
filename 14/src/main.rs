c2bced4cf7534a0cbe8bc2e0733fbf3fbe8f6da8
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-14 11:23:28 +0000

    Hmm, move
    error[E0382]: use of moved value: `iter`
       --> src/main.rs:39:9
        |
    32  |         let iter = i.lines();
        |             ---- move occurs because `iter` has type `std::str::Lines<'_>`, which does not implement the `Copy` trait
    ...
    35  |             .skip_while(|l| l.contains("mask"))
        |              ---------------------------------- `iter` moved due to this method call
    ...
    39  |         iter
        |         ^^^^ value used here after move
        |
    note: this function consumes the receiver `self` by taking ownership of it, which moves `iter`

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,4 +1,22 @@
+// TODO: serde
 use aocf::Aoc;
+
+#[derive(Default)]
+struct Ins {
+    mask: Vec<Option<bool>>,
+    sets: Vec<(u32, u32)>,
+}
+
+impl Ins {
+    fn from_str(s: &str) -> Self {
+        Self::default()
+    }
+
+    fn add_set_str(&mut self, s: &str) -> &mut Self {
+        self.sets.push((1,1));
+        self
+    }
+}
 
 fn main() {
     let mut aoc = Aoc::new()
@@ -10,6 +28,28 @@ fn main() {
     let input = aoc.get_input(false);
 
     if let Ok(i) = input {
+        let mut ins: Vec<Ins> = Vec::new();
+        let iter = i.lines();
 
+        iter
+            .skip_while(|l| l.contains("mask"))
+            .take(1)
+            .for_each(|l| ins.push(Ins::from_str(l)));
+
+        iter
+            .take_while(|l| l.contains("mem"))
+            .for_each(|l| {
+                if let Some(m) = ins.last_mut() {
+                    m.add_set_str(l);
+                }
+            });
+
+
+        part_1();
     }
+}
+
+fn part_1() {
+    let iv = vec![0; 36];
+
 }
