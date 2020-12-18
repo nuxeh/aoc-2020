1b6a9fa9a96908a3c2c8e85c96bd3e1dbd6a603a
Ed Cragg <ed.cragg@eipi.xyz>
2020-12-18 07:44:32 +0000

    Start

--- 18/src/main.rs
+++ 18/src/main.rs
@@ -11,5 +11,29 @@ fn main() {
 
     if let Ok(i) = input {
 
+        let i: Vec<String> = i
+            .lines()
+            .map(|l| l.replace("(", "( "))
+            .map(|l| l.replace(")", " )"))
+            .collect();
+
+        println!("{:?}", i);
+
+        let r: Vec<u32> = i
+            .iter()
+            .map(|l| {
+                l
+                    .split(' ')
+                    .fold(vec![], |mut acc, c| {
+                        match (c.parse::<u32>(), c) {
+                            _ => acc.push(1u32),
+                        };
+                        acc
+                    })[0]
+            })
+            .collect();
+
+        println!("{:?}", r);
+
     }
 }
